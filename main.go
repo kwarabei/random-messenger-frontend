5752cc76e73fd14376ece9bf7c16f589a0d6bcb6
Ascendt <102731455+Ascendt@users.noreply.github.com>
2022-04-01 20:53:57 +0300

    Update main.go

--- main.go
+++ main.go
@@ -1,12 +1,16 @@
 package main
 
+// A simple Gio program. See https://gioui.org for more information.
+//
+// This program showcases markdown rendering.
+// The left pane contains a text editor for inputing raw text.
+// The right pane renders the resulting markdown document using richtext.
+//
+// Richtext is fully interactive, links can be clicked, hovered, and longpressed.
+
 import (
-	"fmt"
 	"image"
-	"image/jpeg"
-	_ "image/jpeg"
-	"image/png"
-	_ "image/png"
+	"image/color"
 	"log"
 	"os"
 
@@ -14,18 +18,30 @@ import (
 	"gioui.org/io/system"
 	"gioui.org/layout"
 	"gioui.org/op"
+	"gioui.org/op/clip"
 	"gioui.org/op/paint"
+	"gioui.org/text"
+	"gioui.org/unit"
 	"gioui.org/widget"
 	"gioui.org/widget/material"
+	"gioui.org/x/component"
+	"gioui.org/x/markdown"
+	"gioui.org/x/richtext"
 
 	"gioui.org/font/gofont"
-	"gioui.org/x/explorer"
+	"github.com/inkeliz/giohyperlink"
 )
 
 func main() {
+	ui := UI{
+		Window:   app.NewWindow(),
+		Renderer: markdown.NewRenderer(),
+		Shaper:   text.NewCache(gofont.Collection()),
+		Theme:    NewTheme(gofont.Collection()),
+		Resize:   component.Resize{Ratio: 0.5},
+	}
 	go func() {
-		w := app.NewWindow()
-		if err := loop(w); err != nil {
+		if err := ui.Loop(); err != nil {
 			log.Fatal(err)
 		}
 		os.Exit(0)
@@ -38,117 +54,113 @@ type (
 	D = layout.Dimensions
 )
 
-// ImageResult is the results of trying to open an image. It may
-// contain either an error or an image, but not both. The error
-// should always be checked first.
-type ImageResult struct {
-	Error  error
-	Format string
-	Image  image.Image
+// UI specifies the user interface.
+type UI struct {
+	// External systems.
+	// Window provides access to the OS window.
+	Window *app.Window
+	// Theme contains semantic style data. Extends `material.Theme`.
+	Theme *Theme
+	// Shaper cache of registered fonts.
+	Shaper *text.Cache
+	// Renderer tranforms raw text containing markdown into richtext.
+	Renderer *markdown.Renderer
+
+	// Core state.
+	// Editor retains raw text in an edit buffer.
+	Editor widget.Editor
+	// TextState retains rich text interactions: clicks, hovers and longpresses.
+	TextState richtext.InteractiveText
+	// Resize state retains the split between the editor and the rendered text.
+	component.Resize
 }
 
-func loop(w *app.Window) error {
-	expl := explorer.NewExplorer(w)
-	var openBtn, saveBtn widget.Clickable
-	th := material.NewTheme(gofont.Collection())
-	imgChan := make(chan ImageResult)
-	saveChan := make(chan error)
-	var img ImageResult
-	var saveErr error
+// Theme contains semantic style data.
+type Theme struct {
+	// Base theme to extend.
+	Base *material.Theme
+	// cache of processed markdown.
+	cache []richtext.SpanStyle
+}
+
+// NewTheme instantiates a theme, extending material theme.
+func NewTheme(font []text.FontFace) *Theme {
+	return &Theme{
+		Base: material.NewTheme(font),
+	}
+}
+
+// Loop drives the UI until the window is destroyed.
+func (ui UI) Loop() error {
 	var ops op.Ops
 	for {
-		select {
-		case img = <-imgChan:
-			w.Invalidate()
-		case saveErr = <-saveChan:
-			w.Invalidate()
-		case e := <-w.Events():
-			expl.ListenEvents(e)
-			switch e := e.(type) {
-			case system.DestroyEvent:
-				return e.Err
-			case system.FrameEvent:
-				gtx := layout.NewContext(&ops, e)
-				if openBtn.Clicked() {
-					go func() {
-						file, err := expl.ChooseFile("png", "jpeg", "jpg")
-						if err != nil {
-							err = fmt.Errorf("failed opening image file: %w", err)
-							imgChan <- ImageResult{Error: err}
-							return
-						}
-						defer file.Close()
-						imgData, format, err := image.Decode(file)
-						if err != nil {
-							err = fmt.Errorf("failed decoding image data: %w", err)
-							imgChan <- ImageResult{Error: err}
-							return
-						}
-						imgChan <- ImageResult{Image: imgData, Format: format}
-					}()
-				}
-				if saveBtn.Clicked() {
-					go func(img ImageResult) {
-						if img.Error != nil {
-							saveChan <- fmt.Errorf("no image loaded, cannot save")
-							return
-						}
-						extension := "jpg"
-						switch img.Format {
-						case "png":
-							extension = "png"
-						}
-						file, err := expl.CreateFile("file." + extension)
-						if err != nil {
-							saveChan <- fmt.Errorf("failed exporting image file: %w", err)
-							return
-						}
-						defer func() {
-							saveChan <- file.Close()
-						}()
-						switch extension {
-						case "jpg":
-							if err := jpeg.Encode(file, img.Image, nil); err != nil {
-								saveChan <- fmt.Errorf("failed encoding image file: %w", err)
-								return
-							}
-						case "png":
-							if err := png.Encode(file, img.Image); err != nil {
-								saveChan <- fmt.Errorf("failed encoding image file: %w", err)
-								return
-							}
-						}
-					}(img)
-				}
-				layout.Flex{Axis: layout.Vertical}.Layout(gtx,
-					layout.Rigid(material.Button(th, &openBtn, "Open Image").Layout),
-					layout.Flexed(1, func(gtx C) D {
-						if img.Error == nil && img.Image == nil {
-							return D{}
-						} else if img.Error != nil {
-							return material.H6(th, img.Error.Error()).Layout(gtx)
-						}
+		e := <-ui.Window.Events()
+		giohyperlink.ListenEvents(e)
+		switch e := e.(type) {
+		case system.DestroyEvent:
+			return e.Err
+		case system.FrameEvent:
+			gtx := layout.NewContext(&ops, e)
+			ui.Layout(gtx)
+			e.Frame(gtx.Ops)
+		}
+	}
+}
 
-						return widget.Image{
-							Src: paint.NewImageOp(img.Image),
-							Fit: widget.Contain,
-						}.Layout(gtx)
-					}),
-					layout.Rigid(func(gtx C) D {
-						if img.Image == nil {
-							gtx.Queue = nil
-						}
-						return material.Button(th, &saveBtn, "Save Image").Layout(gtx)
-					}),
-					layout.Rigid(func(gtx C) D {
-						if saveErr == nil {
-							return D{}
-						}
-						return material.H6(th, saveErr.Error()).Layout(gtx)
-					}),
-				)
-				e.Frame(gtx.Ops)
+// Update processes events from the previous frame, updating state accordingly.
+func (ui *UI) Update(gtx C) {
+	for o, events := ui.TextState.Events(); o != nil; o, events = ui.TextState.Events() {
+		for _, e := range events {
+			switch e.Type {
+			case richtext.Click:
+				if url := o.Get(markdown.MetadataURL); url != "" {
+					if err := giohyperlink.Open(url); err != nil {
+						// TODO(jfm): display UI element explaining the error to the user.
+						log.Printf("error: opening hyperlink: %v", err)
+					}
+				}
+			case richtext.Hover:
+			case richtext.LongPress:
+				log.Println("longpress")
+				ui.Window.Option(app.Title(o.Get(markdown.MetadataURL)))
 			}
 		}
 	}
+	for _, event := range ui.Editor.Events() {
+		if _, ok := event.(widget.ChangeEvent); ok {
+			var err error
+			ui.Theme.cache, err = ui.Renderer.Render(ui.Theme.Base, []byte(ui.Editor.Text()))
+			if err != nil {
+				// TODO(jfm): display UI element explaining the error to the user.
+				log.Printf("error: rendering markdown: %v", err)
+			}
+		}
+	}
+}
+
+// Layout renders the current frame.
+func (ui *UI) Layout(gtx C) D {
+	ui.Update(gtx)
+	return ui.Resize.Layout(gtx,
+		func(gtx C) D {
+			return layout.UniformInset(unit.Dp(4)).Layout(gtx, func(gtx C) D {
+				return material.Editor(ui.Theme.Base, &ui.Editor, "markdown").Layout(gtx)
+			})
+		},
+		func(gtx C) D {
+			return layout.UniformInset(unit.Dp(4)).Layout(gtx, func(gtx C) D {
+				return richtext.Text(&ui.TextState, ui.Shaper, ui.Theme.cache...).Layout(gtx)
+			})
+		},
+		func(gtx C) D {
+			rect := image.Rectangle{
+				Max: image.Point{
+					X: (gtx.Px(unit.Dp(4))),
+					Y: (gtx.Constraints.Max.Y),
+				},
+			}
+			paint.FillShape(gtx.Ops, color.NRGBA{A: 200}, clip.Rect(rect).Op())
+			return D{Size: rect.Max}
+		},
+	)
 }
